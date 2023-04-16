b2c09fa84d8294a9296186729aa1ac3e33982a87
Manuel Gandul <manu.gandul@gmail.com>
2023-04-16 23:24:00 +0200

    Create CRUD for feed

--- src/App.ts
+++ src/App.ts
@@ -1,18 +1,28 @@
-import express from 'express';
-import { PORT } from '@config/Envs';
-import { connect } from '@database/Mongo';
+import express, { NextFunction, Request, Response } from 'express';
 import './env';
+import { PORT, SECURITY_HEADER } from '@config/Envs';
+import { connect } from '@database/Mongo';
+import { NoticeRouter } from '@routes/Notice';
 
 const app = express();
-const port = PORT;
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  if (SECURITY_HEADER && req.headers.security !== SECURITY_HEADER) {
+    res.status(401).send('Unauthorized request');
+  } else {
+    next();
+  }
+});
 
 app.use(express.json());
 
-app.get('/', (req, res): void => {
+app.use('/notice', NoticeRouter);
+
+app.get('/', (req: Request, res: Response): void => {
   res.send('Hello World!');
 });
 
-app.listen(port, async () => {
+app.listen(PORT, async () => {
   await connect();
-  return console.log(`server is listening on ${port}`);
+  return console.log(`server is listening on ${PORT}`);
 });
--- src/config/Envs.ts
+++ src/config/Envs.ts
@@ -2,3 +2,5 @@
 export const PORT = Number(process.env.PORT) || 3000;
 // Limits the max notices per newspaper, default is 5
 export const MAX_NEWSPAPER_NOTICES: number = Number(process.env.MAX_NEWSPAPER_NOTICES) || 5;
+// Security header
+export const SECURITY_HEADER = process.env.SECURITY_HEADER;
